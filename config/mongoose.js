3717e200da9703efa0359de32950aaa4c66d332f
AniketRathod305 <aniketrathod2000@gmail.com>
2021-04-21 16:51:27 +0530

    aniket

--- config/mongoose.js
+++ config/mongoose.js
@@ -14,7 +14,7 @@ const db = mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
 
 db.once('open',function(){
-    console.log('Connected to Database :: MongoDB');
+    console.log('Connected to Database');
 });
 
 module.exports = db;
